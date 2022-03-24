608eb2c2205b67e546bddd7a5371720563f0a0bd
zishan <mactyller99@gmail.com>
2022-03-24 22:51:16 +0530

    js files are added

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+var name = "hii";
+var age=20;
+var $age1="20";
+var _i="i";
+console.log(_i);
+if(age==$age1)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}
+console.log(name);
+console.log(age);
