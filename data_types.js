28b4a0e9dc5247e2aa310bfed48f2b0f8201337e
zishan <mactyller99@gmail.com>
2022-05-10 00:17:17 +0530

    js files are added

---
+++ data_types.js
@@ -0,0 +1,55 @@
+var name="zedi";
+console.log(typeof(name));
+
+
+var myage=20;
+console.log(typeof(myage));
+
+console.log(typeof(typeof(myage)));
+
+
+
+var bool=true;
+console.log(typeof(bool));
+
+console.log("------->Exercise<--------");
+
+var n1=10+"20";
+
+console.log(n1);
+
+var n2=9-"5";//bug
+console.log(n2); 
+
+var n3="java"+"script";
+console.log(n3);
+
+var n4=" "+0;
+console.log(n4);
+
+
+var n5="zedi"-"alpha";
+console.log(n5);
+
+
+// 1 ===> true
+//0  ===> false
+
+
+
+var n6=true+true;
+console.log(n6);
+
+
+var n7=true+false;
+console.log(n7);
+
+var n8=false+true;
+console.log(n8);
+
+
+var n9=false-true;
+console.log(n9);
+
+
+
---
+++ index.js
@@ -0,0 +1,24 @@
+
+<<<<<<< HEAD
+var name = "hii";
+var age=20;
+var $age1="20";
+var _i="i";
+console.log(_i);
+if(age==$age1)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}
+var _1u="o";
+// var tu%=8990;
+// console.log(tu%);
+console.log(_1u);
+console.log(name);
+console.log(age);
+=======
+console.log('hello world');
+>>>>>>> 5fd9122 (next js files are added)
